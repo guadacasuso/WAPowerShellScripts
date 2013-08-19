2cba46e356be077d0f9780d71ca0796d52fec376
Guada <guada.casuso@gmail.com>
2013-08-19 15:55:23 -0400

    Update CreateWindowsVM.ps1

--- CreateWindowsVM.ps1
+++ CreateWindowsVM.ps1
@@ -11,6 +11,16 @@
     This example creates a new Windows VM 
 #>
 
+<#param(
+    [CmdletBinding( SupportsShouldProcess=$true)]
+         
+    # The webSite Name you want to create
+    [Parameter(Mandatory = $true)] 
+    [string]$WebSiteName
+          
+    )
+#>
+
 
 # Mark the start time of the script execution
 $startTime = Get-Date
@@ -19,7 +29,9 @@ Set-StrictMode -Version 3
 $VerbosePreference = "Continue"
 $ErrorActionPreference = "Stop"
 
-$images = Get-AzureVMImage
+#Get the images of Windows Server availables
+$images = Get-AzureVMImage `
+| where { $_.ImageFamily -eq “Windows Server 2012 Datacenter” } 
 
 $locations = Get-AzureLocation
 
@@ -30,22 +42,24 @@ $serviceExist= Test-AzureName -Name $mySvc -Service
 
 if($serviceExist)
 {
-    throw "The service already exists"
+    throw "The service already exists - Please select a new one"
     exit; 
 }
 else
 {
 
     $myPwd = "P@ssw0rd"
-    $userName = "User"
+    $userName = "username"
 
     #Be sure to select a Windows Server Image from the list (Ex $images[80].imagename) 
     #Consider the  Source image location to be the same one than the one of the affinity group
 
-    New-AzureQuickVM -Windows -name "MyWinVM" -ImageName $images[80].imagename  -ServiceName $mySvc -Location $locations[6].name -AdminUsername $userName -Password $myPwd
+    New-AzureQuickVM -Windows -name "MyVM" -ImageName $images[0].imagename  -ServiceName $mySvc `
+                    -Location $locations[6].name -AdminUsername $userName -Password $myPwd
 
 
 }
+
 $finishTime= Get-Date
 
 Write-Verbose ("Total time used (seconds): {0}" -f ($finishTime - $startTime).TotalSeconds)
